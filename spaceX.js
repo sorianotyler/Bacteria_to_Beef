571d898e6463622eb56aaea3d3061f78ab817e32
Tyler Soriano <soriano.tyler@gmail.com>
2022-04-29 12:26:31 -0700

    finished deliverable 1

--- spaceX.js
+++ spaceX.js
@@ -26,6 +26,8 @@ d3.json("samples.json").then(function(data){
 
 d3.json("samples.json").then(function(data){
     wfreq = data.metadata.map(person =>
-person.wfreq).sort((a,b) => b - a);
+    person.wfreq).sort((a,b) => b - a);
     console.log(wfreq);
 });
+
+
